351bbe5c1e22b3e89401c5d1390a0e7f2e4341a6
Educg550 <eduardo9cruz@gmail.com>
2026-05-23 17:51:12 -0300

    feat(seo): dynamic icon (favicon) generation

---
+++ app/icon.tsx
@@ -0,0 +1,28 @@
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+export const size = { width: 32, height: 32 };
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        background: "#272822",
+        color: "#F92672",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "serif",
+        fontStyle: "italic",
+        fontSize: 24,
+        fontWeight: 700,
+      }}
+    >
+      d
+    </div>,
+    { ...size },
+  );
+}
