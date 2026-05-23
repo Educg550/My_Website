9ed0eebe3cdfe09b641ffcf6b5cfcf61452b0642
Educg550 <eduardo9cruz@gmail.com>
2026-05-23 16:48:01 -0300

    feat(theme): wire Monokai tokens into Tailwind v4 @theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
